7a00d1c85ffd6097f3a8970abb6724e966cd1ff5
CharLemAznable <545541819@qq.com>
2022-03-11 12:22:26 +0800

    Add ConfigFactory#configLoader.

--- src/main/java/com/github/charlemaznable/configservice/ConfigFactory.java
+++ src/main/java/com/github/charlemaznable/configservice/ConfigFactory.java
@@ -1,9 +1,11 @@
 package com.github.charlemaznable.configservice;
 
+import com.github.charlemaznable.core.context.FactoryContext;
+import com.github.charlemaznable.core.lang.Factory;
 import lombok.NoArgsConstructor;
 
-import static com.github.charlemaznable.configservice.apollo.ApolloFactory.getApollo;
-import static com.github.charlemaznable.configservice.diamond.DiamondFactory.getDiamond;
+import static com.github.charlemaznable.configservice.apollo.ApolloFactory.apolloLoader;
+import static com.github.charlemaznable.configservice.diamond.DiamondFactory.diamondLoader;
 import static com.github.charlemaznable.configservice.elf.ConfigServiceElf.substitute;
 import static lombok.AccessLevel.PRIVATE;
 
@@ -13,9 +15,13 @@ public final class ConfigFactory {
     private static final String ENV_SOURCE = "${ConfigService:-diamond}";
 
     public static <T> T getConfig(Class<T> configClass) {
+        return configLoader(FactoryContext.get()).getConfig(configClass);
+    }
+
+    public static ConfigLoader configLoader(Factory factory) {
         if ("apollo".equalsIgnoreCase(substitute(ENV_SOURCE))) {
-            return getApollo(configClass);
+            return apolloLoader(factory);
         }
-        return getDiamond(configClass);
+        return diamondLoader(factory);
     }
 }
