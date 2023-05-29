22850cb072d09e16874de55053ac6b64744ae9f6
CharLemAznable <545541819@qq.com>
2023-05-29 15:48:29 +0800

    Fixed duplication.

--- src/main/java/com/github/charlemaznable/configservice/etcd/EtcdConfigGetter.java
+++ src/main/java/com/github/charlemaznable/configservice/etcd/EtcdConfigGetter.java
@@ -1,75 +1,19 @@
 package com.github.charlemaznable.configservice.etcd;
 
-import com.ctrip.framework.apollo.util.function.Functions;
-import com.github.charlemaznable.configservice.impl.DefaultConfigGetter;
+import com.github.charlemaznable.configservice.impl.DefaultPropertyConfigGetter;
 import com.github.charlemaznable.etcdconf.EtcdConfig;
 import com.github.charlemaznable.etcdconf.EtcdConfigService;
 import lombok.AllArgsConstructor;
-import org.apache.commons.lang3.BooleanUtils;
 
 import java.util.Properties;
-import java.util.function.Function;
 
 import static com.github.charlemaznable.configservice.elf.ConfigServiceElf.parseStringToProperties;
-import static com.github.charlemaznable.core.lang.Condition.checkBlank;
 import static com.github.charlemaznable.core.lang.Condition.checkNull;
 import static com.github.charlemaznable.core.lang.Condition.notNullThen;
 import static com.github.charlemaznable.core.lang.Str.isBlank;
 import static org.apache.commons.text.StringSubstitutor.replace;
 
-public abstract class EtcdConfigGetter extends DefaultConfigGetter {
-
-    public abstract String getPropertyValue(String namespace, String propertyName);
-
-    @Override
-    public int getInt(String key, int defaultValue) {
-        return getValue(key, defaultValue, Functions.TO_INT_FUNCTION);
-    }
-
-    @Override
-    public long getLong(String key, long defaultValue) {
-        return getValue(key, defaultValue, Functions.TO_LONG_FUNCTION);
-    }
-
-    @Override
-    public short getShort(String key, short defaultValue) {
-        return getValue(key, defaultValue, Functions.TO_SHORT_FUNCTION);
-    }
-
-    @Override
-    public float getFloat(String key, float defaultValue) {
-        return getValue(key, defaultValue, Functions.TO_FLOAT_FUNCTION);
-    }
-
-    @Override
-    public double getDouble(String key, double defaultValue) {
-        return getValue(key, defaultValue, Functions.TO_DOUBLE_FUNCTION);
-    }
-
-    @Override
-    public byte getByte(String key, byte defaultValue) {
-        return getValue(key, defaultValue, Functions.TO_BYTE_FUNCTION);
-    }
-
-    @Override
-    public boolean getBoolean(String key, boolean defaultValue) {
-        return getValue(key, defaultValue, BooleanUtils::toBoolean);
-    }
-
-    @Override
-    public long getDuration(String key, long defaultValue) {
-        return getValue(key, defaultValue, Functions.TO_DURATION_FUNCTION);
-    }
-
-    private <T> T getValue(String key, T defaultValue, Function<String, T> parser) {
-        return checkBlank(getString(key), () -> defaultValue, value -> {
-            try {
-                return parser.apply(value);
-            } catch (Exception e) {
-                return defaultValue;
-            }
-        });
-    }
+public abstract class EtcdConfigGetter extends DefaultPropertyConfigGetter {
 
     @AllArgsConstructor
     static final class EtcdNamespaceGetter extends EtcdConfigGetter {
---
+++ src/main/java/com/github/charlemaznable/configservice/impl/DefaultPropertyConfigGetter.java
@@ -0,0 +1,63 @@
+package com.github.charlemaznable.configservice.impl;
+
+import com.ctrip.framework.apollo.util.function.Functions;
+import org.apache.commons.lang3.BooleanUtils;
+
+import java.util.function.Function;
+
+import static com.github.charlemaznable.core.lang.Condition.checkBlank;
+
+public abstract class DefaultPropertyConfigGetter extends DefaultConfigGetter {
+
+    public abstract String getPropertyValue(String keyset, String key);
+
+    @Override
+    public int getInt(String key, int defaultValue) {
+        return getValue(key, defaultValue, Functions.TO_INT_FUNCTION);
+    }
+
+    @Override
+    public long getLong(String key, long defaultValue) {
+        return getValue(key, defaultValue, Functions.TO_LONG_FUNCTION);
+    }
+
+    @Override
+    public short getShort(String key, short defaultValue) {
+        return getValue(key, defaultValue, Functions.TO_SHORT_FUNCTION);
+    }
+
+    @Override
+    public float getFloat(String key, float defaultValue) {
+        return getValue(key, defaultValue, Functions.TO_FLOAT_FUNCTION);
+    }
+
+    @Override
+    public double getDouble(String key, double defaultValue) {
+        return getValue(key, defaultValue, Functions.TO_DOUBLE_FUNCTION);
+    }
+
+    @Override
+    public byte getByte(String key, byte defaultValue) {
+        return getValue(key, defaultValue, Functions.TO_BYTE_FUNCTION);
+    }
+
+    @Override
+    public boolean getBoolean(String key, boolean defaultValue) {
+        return getValue(key, defaultValue, BooleanUtils::toBoolean);
+    }
+
+    @Override
+    public long getDuration(String key, long defaultValue) {
+        return getValue(key, defaultValue, Functions.TO_DURATION_FUNCTION);
+    }
+
+    private <T> T getValue(String key, T defaultValue, Function<String, T> parser) {
+        return checkBlank(getString(key), () -> defaultValue, value -> {
+            try {
+                return parser.apply(value);
+            } catch (Exception e) {
+                return defaultValue;
+            }
+        });
+    }
+}
